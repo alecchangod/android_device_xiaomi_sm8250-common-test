40ab42e44c92db5cc55faeb901c8320848025025
AndroidHQ254 <d.murimi14@gmail.com>
2022-09-17 05:57:11 +0000

    sm8250-common: Build missing sensor packages
    I/hwservicemanager(  695): getTransport: Cannot find entry android.hardware.sensors@2.1::ISensors/default in either framework or device VINTF manifest.
    I/hwservicemanager(  695): getTransport: Cannot find entry android.hardware.sensors@2.0::ISensors/default in either framework or device VINTF manifest.

--- common.mk
+++ common.mk
@@ -476,7 +476,10 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl-xiaomi \
-    android.hardware.sensors@1.0-service
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
