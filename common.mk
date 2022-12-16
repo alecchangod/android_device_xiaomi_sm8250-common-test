590a0eaaabbc0f48a6fbfdf84ad381589b018841
Luofan Chen <clfbbn@gmail.com>
2022-12-16 22:56:41 +0530

    sm8250-common: Build android.system.keystore2
    Fixes: Could not find 'aidl/android.system.keystore2.IKeystoreService/default'
           for ctl.interface_start from pid: 617 (/system/bin/servicemanager)

--- common.mk
+++ common.mk
@@ -319,6 +319,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
