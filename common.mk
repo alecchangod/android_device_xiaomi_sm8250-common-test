6168106c6a9404f0079f53d81a15c086d6adcc9d
Alec Chan <alec3d02@gmail.com>
2023-10-21 10:14:56 +0800

    sm8250-common: uprev audio to 7.1
    10-11 19:25:10.714   588   588 I hwservicemanager: getTransport: Cannot find entry android.hardware.audio.effect@7.1::IEffectsFactory/default in either framework or device VINTF manifest.
    10-11 19:25:10.715   588   588 I hwservicemanager: getTransport: Cannot find entry android.hardware.audio.effect@7.0::IEffectsFactory/default in either framework or device VINTF manifest.
    10-11 19:25:10.715   588   588 I hwservicemanager: getTransport: Cannot find entry android.hardware.audio@7.1::IDevicesFactory/default in either framework or device VINTF manifest.
    10-11 19:25:10.715   588   588 I hwservicemanager: getTransport: Cannot find entry android.hardware.audio@7.0::IDevicesFactory/default in either framework or device VINTF manifest.

--- common.mk
+++ common.mk
@@ -101,8 +101,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.effect@7.1-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl
